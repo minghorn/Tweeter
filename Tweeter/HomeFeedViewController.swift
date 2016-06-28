0ba942125829f6c8838768d3ab38692307ac3273
Ming Horn <minghaihorn@gmail.com>
2016-06-28 14:02:37 -0700

    Updating organization

--- Tweeter/HomeFeedViewController.swift
+++ Tweeter/HomeFeedViewController.swift
@@ -8,39 +8,43 @@
 
 import UIKit
 
-class HomeFeedViewController: UIViewController, UITableViewDelegate {
+class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     var tweets: [Tweet]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        
+        //Request tweets and put them into an array
         TwitterClient.sharedInstance.homeTimeline({ (tweets:[Tweet]) in
             self.tweets = tweets
             self.tableView.reloadData()
         }) { (error: NSError) in
             print(error.localizedDescription)
         }
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func onLogOut(sender: AnyObject) {
         TwitterClient.sharedInstance.logOut()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let tweets = tweets {
+            return tweets.count
+        } else {
+            return 0
+        }
     }
-    */
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tweetCell", forIndexPath: indexPath) as! TweetCell
+        return cell
+    }
+    
 }
---
+++ Tweeter/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Tweeter
+//
+//  Created by Ming Horn on 6/28/16.
+//  Copyright © 2016 Ming Horn. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
